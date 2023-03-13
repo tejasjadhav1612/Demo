1e2aa01b2f8cc9106ba141cc44115dee27f304a4
tejasjadhav1612 <tejasjadhav1612@gmail.com>
2023-03-13 22:07:27 +0530

    adding reload feature after submitting

--- src/Component/UserTask/UserList.js
+++ src/Component/UserTask/UserList.js
@@ -3,84 +3,91 @@ import MUIDataTable from 'mui-datatables';
 import React, { useEffect, useState } from 'react';
 import AddEditUser from './AddEditUser';
 import Button from "@mui/material/Button";
+import UserContext from './UserContext';
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
     const [openDialog, setOpenDialog] = useState(false);
 
-    const handleClose = ()=>{
+    const handleClose = () => {
         setOpenDialog(false);
+    };
+
+    const loadUsers = () => {
+        axios
+            .get("http://localhost:8080/users")
+            .then((response) => {
+                setUsers(response.data);
+
+            })
+            .catch(console.log);
     }
 
-    useEffect(()=>{
-        axios
-        .get("http://localhost:8080/users")
-        .then((response)=>{
-            setUsers(response.data);
-
-        })
-        .catch(console.log);
-    });
+    useEffect(() => {
+        loadUsers();
+    }, []);
 
     const columns = [
         {
-            name:"id",
-            label:"ID",
-            options:{
-                sort:true,
-                filter:false,
+            name: "id",
+            label: "ID",
+            options: {
+                sort: true,
+                filter: false,
             },
 
         },
         {
-            name:"mobile",
-            label:"Mobile ",
-            options:{
-                sort:true,
-                filter:false,
+            name: "mobile",
+            label: "Mobile ",
+            options: {
+                sort: true,
+                filter: false,
             },
 
         },
         {
-            name:"email",
-            label:"Email",
-            options:{
-                sort:true,
-                filter:false,
+            name: "email",
+            label: "Email",
+            options: {
+                sort: true,
+                filter: false,
             },
 
         },
         {
-            name:"city",
-            label:"City",
-            options:{
-                sort:true,
-                filter:true,
+            name: "city",
+            label: "City",
+            options: {
+                sort: true,
+                filter: true,
             },
 
         },
         {
-            name:"gender",
-            label:"Gender",
-            options:{
-                sort:true,
-                filter:true,
+            name: "gender",
+            label: "Gender",
+            options: {
+                sort: true,
+                filter: true,
             },
 
         },
-        
+
     ];
 
-    const addUser = () =>{
+    const addUser = () => {
         setOpenDialog(true);
     };
-    return ( 
+    return (
         <>
-        <AddEditUser open={openDialog} handleClose={handleClose}/>
-        <Button onClick={addUser}>New +</Button>
-        <MUIDataTable title="User List" data={users} columns={columns}/>
+            <UserContext.Provider value={{loadUsers, handleClose}}>
+                <AddEditUser open={openDialog} handleClose={handleClose} />
+            </UserContext.Provider>
+            <Button onClick={addUser}>New +</Button>
+            <MUIDataTable title="User List" data={users} columns={columns} />
         </>
-     );
+    );
 }
- 
+
 export default UserList;
